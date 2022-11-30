a2665830f32e6e568e1c58337bd99ff01b18caf1
arex388 <31530399+arex388@users.noreply.github.com>
2022-11-30 12:28:39 -0700

    Minor namespace fix for extensions

--- Arex388.GoogleMaps/Extensions/GoogleMapsClientExtensions.cs
+++ Arex388.GoogleMaps/Extensions/GoogleMapsClientExtensions.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Arex388.GoogleMaps.Extensions;
+namespace Arex388.GoogleMaps;
 
 /// <summary>
 /// Google Maps API client extensions.
